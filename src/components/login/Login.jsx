448f1f010970a3a0e02dcc07eef704621cac9342
Saayemm <sayemdcc@gmail.com>
2025-01-18 14:54:29 +0600

    conditional btn

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,11 @@
-import { getAuth ,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth ,GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+
+    const [user, setUser] = useState(null)
+
     const auth = getAuth(app);
     console.log(app)
     const provider = new GoogleAuthProvider();
@@ -9,8 +13,20 @@ const Login = () => {
     const hundleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
         .then(result=> {
-            const user = result.user;
-            console.log(user)
+            const loggedInUser = result.user;
+            console.log(loggedInUser)
+            setUser(loggedInUser)
+        })
+        .catch(error=> {
+            console.log("error", error.message)
+        })
+    }
+
+    const hundleGoogleSignOut = () => {
+        signOut(auth)
+        .then(result=> {
+            console.log(result)
+            setUser(null)
         })
         .catch(error=> {
             console.log("error", error.message)
@@ -19,7 +35,17 @@ const Login = () => {
   return (
     <div className="flex flex-col m-auto gap-3">
         <h2>Welcome to Login Page</h2>
-        <button className="bg-green-700 text-white px-6 py-2 rounded-sm" onClick={hundleGoogleSignIn}>Google Login</button>
+       {
+        user ?  
+        <button className="bg-red-700 text-white px-6 py-2 rounded-sm" onClick={hundleGoogleSignOut}>Sign Out</button> :
+        <button className="bg-green-700 text-white px-6 py-2 rounded-sm" onClick={hundleGoogleSignIn}>Sign In</button> 
+       }
+       {
+        user && <div>
+             <h2>{user.displayName}</h2>
+             <img src={user.photoURL} alt="" />
+        </div>
+       }
     </div>
   )
 }
